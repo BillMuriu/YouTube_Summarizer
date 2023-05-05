c344a1bdab2c80064dbbf0780e10621e4bf72196
unknown <njorogekamau63@gmail.com>
2023-05-05 23:15:59 +0300

    import api_view and response from rest_framework

--- YouTube_Sum/api/views.py
+++ YouTube_Sum/api/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 # Create your views here.
 
+@api_view(['POST'])
 def apiOverview(request):
-    return JsonResponse("API BASE POINT", safe=False)
+    api_urls = {
+        'List': '/task-list',
+    }
+    return Response(api_urls)
